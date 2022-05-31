85d51ffbb3e7228c168df27842eb6a556879565a
hackschnitzel09 <kevinross95@gmx.net>
2022-05-31 22:10:13 +0200

    add broadcast support

--- client.py
+++ client.py
@@ -2,22 +2,39 @@ import socket
 
 #create UDP socket
 udp_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-
-#get my ip and set port
+#get my ip
 hostname = socket.gethostname()
-server_ip = socket.gethostbyname(hostname)
-server_port =10001
+myip = socket.gethostbyname(hostname)
 
-#set buffer
-buffer_size = 1024
+broadcast_msg = myip + "online"
+broadcast_ip = "192.168.56.255"
+broadcast_port = 5961
 
-msg="hi! I am client it works!"
+def client(server_ip, server_port):
+    #get my ip and set port
+    hostname = socket.gethostname()
+    server_ip = "xxx.xxx.xxx.xxx"
+    server_port =10001
 
-#send my data
-udp_socket.sendto(msg.encode(),(server_ip, server_port))
-print("Send: ", msg)
+    #set buffer
+    buffer_size = 1024
 
-#Receive response
-print("wait for server...")
-data, server = udp_socket.recvfrom(buffer_size)
-print("received: ", data.decode())
+    msg="hi! I am client it works!"
+
+    #send my data
+    udp_socket.sendto(msg.encode(),(server_ip, server_port))
+    print("Send: ", msg)
+
+    #Receive response
+    print("wait for server...")
+    data, server = udp_socket.recvfrom(buffer_size)
+    print("received: ", data.decode())
+
+#broadcast sender
+def broadcast_send(broadcast_msg, broadcast_ip, proadcast_port):
+    #create socket
+    broadcast_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    broadcast_socket.sendto(str.encode(broadcast_msg), (broadcast_ip, broadcast_port))
+    print("broadcast msg send")
+
+broadcast_send(broadcast_msg, broadcast_ip, broadcast_port)
--- server.py
+++ server.py
@@ -6,24 +6,50 @@ udp_socket= socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 #get my ip and set port
 hostname = socket.gethostname()
 myip = socket.gethostbyname(hostname)
+print("my ip is: ", myip)
 myport =10001
 
-#set buffer
-buffer_size = 1024
+#brodcast listerner
+broadcast_port = 5961
 
-msg="hi! I am server, it works!"
+def brodcast_listener(broadcast_port, myip):
+    # Create a UDP socket
+    listen_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    # Set the socket to broadcast and enable reusing addresses
+    listen_socket.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
+    listen_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+    # Bind socket to address and port
+    listen_socket.bind((myip, broadcast_port))
+    print("Listening to broadcast messages")
+    while True:
+        data, addr = listen_socket.recvfrom(1024)
+        if data:
+            print("Received broadcast message:", data.decode())
 
-#bind socket to port
-udp_socket.bind((myip, myport))
-print("Server up and running {}:{}".format(myip, myport))
 
-while True:
-    print("\n waiting for msgs...\n")
 
-    data, client_ip = udp_socket.recvfrom(buffer_size)
-    print("client ip: ", client_ip)
-    print("msg: ", data.decode())
 
-    if data:
-        udp_socket.sendto(str.encode(msg), client_ip)
-        print("repied: ", msg)
+
+def server(hostname, myip, myport):
+    #set buffer
+    buffer_size = 1024
+
+    msg="hi! I am server, it works!"
+
+    #bind socket to port
+    udp_socket.bind((myip, myport))
+    print("Server up and running {}:{}".format(myip, myport))
+
+    while True:
+        print("\n waiting for msgs...\n")
+
+        data, client_ip = udp_socket.recvfrom(buffer_size)
+        print("client ip: ", client_ip)
+        print("msg: ", data.decode())
+
+        if data:
+            udp_socket.sendto(str.encode(msg), client_ip)
+            print("repied: ", msg)
+
+
+brodcast_listener(broadcast_port, myip)
