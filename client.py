b18abedf9cae8e6ede5bea785d9e6a024cf9d37d
hackschnitzel09 <kevinross95@gmx.net>
2022-06-20 18:17:22 +0200

    forward with usr_list integrated

--- client.py
+++ client.py
@@ -1,29 +1,32 @@
 import utility
 
+leader_ip = "192.168.1.63"
 
 to = input("an wen? ")
 msg = input("nachricht? ")
 
-if to == "1":
-    ip = "192.168.0.172"
+msg = utility.usr_ip(to) + "@" + msg
+print(msg)
+# if to == "1":
+#     ip = "192.168.0.172"
 
-elif to == "2":
-    ip = "192.168.0.216"
+# elif to == "2":
+#     ip = "192.168.0.216"
 
-elif to == "3":
-    ip = "192.168.0.249"
+# elif to == "3":
+#     ip = "192.168.0.249"
 
-elif to == "4":
-    ip = "192.168.1.63"
+# elif to == "4":
+#     ip = "192.168.1.63"
 
-elif to == "5":
-    ip = "192.168.1.160"
+# elif to == "5":
+#     ip = "192.168.1.160"
 
-elif to == "6":
-    ip = "192.168.1.167"
+# elif to == "6":
+#     ip = "192.168.1.167"
 
-else:
-    ip = "192.168.1.255"
+# else:
+#     ip = "192.168.1.255"
 
-utility.send_msg(msg, ip)
+utility.send_msg(msg, leader_ip)
 #utility.send_broadcast(msg)
